8048b0b8d7b8dc6f9c41d02ebb2c361c9ee190f7
gregt <greg_tomney@trendmicro.com>
2020-08-31 16:07:06 -0400

    create simple Go webapp with Docker

--- GetAllQuestions/index.js
+++
@@ -1,9 +0,0 @@
-const questions = require("../questions");
-
-module.exports = async function(context, req) {
-  context.log("Request for all questions");
-
-  context.res = {
-    body: questions
-  };
-};
